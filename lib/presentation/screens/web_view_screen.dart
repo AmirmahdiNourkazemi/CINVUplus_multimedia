0cccc8c94f51c2ad9977107bd770acd791563a44
Sormeyli <mohammadhadisormeyli@gmail.com>
2023-05-02 21:55:49 +0430

    open site with bug

--- lib/presentation/screens/web_view_screen.dart
+++ lib/presentation/screens/web_view_screen.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WebViewScreen extends StatefulWidget {
-
   final String url;
 
   const WebViewScreen({Key? key, required this.url}) : super(key: key);
@@ -14,9 +16,51 @@ class WebViewScreen extends StatefulWidget {
 class _WebViewScreenState extends State<WebViewScreen> {
 
   @override
+  void initState() {
+    super.initState();
+
+    _launchUrl();
+  }
+
+
+  Future<void> _launchUrl() async {
+    if (await hasNetwork()) {
+      Uri url = Uri.parse(widget.url);
+      if (!await launchUrl(url)) {
+        Fluttertoast.showToast(
+            msg: "Problem!!",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    } else {
+      Fluttertoast.showToast(
+          msg: "Please check your internet.",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+  }
+
+  Future<bool> hasNetwork() async {
+    try {
+      final result = await InternetAddress.lookup('example.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return  WebviewScaffold(
-        url: widget.url,
+    return Scaffold(
+      body: Container(
+      ),
     );
   }
 }
--- lib/presentation/widgets/feature_item_widget.dart
+++ lib/presentation/widgets/feature_item_widget.dart
@@ -1,45 +1,13 @@
-import 'dart:io';
-
 import 'package:connectplus/config/theme.dart';
 import 'package:connectplus/domain/entities/feature.dart';
 import 'package:connectplus/presentation/screens/web_view_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class FeatureItemWidget extends StatelessWidget {
   final Feature feature;
 
   const FeatureItemWidget({super.key, required this.feature});
-
-  Future<void> _launchUrl() async {
-    if (feature.isEnable && feature.siteUrl != null) {
-      if (await hasNetwork()) {
-        Uri url = Uri.parse(feature.siteUrl!);
-        if (!await launchUrl(url)) {
-          throw Exception('Could not launch $url');
-        }
-      } else {
-        Fluttertoast.showToast(
-            msg: "Please check your internet.",
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.CENTER,
-            timeInSecForIosWeb: 1,
-            textColor: Colors.white,
-            fontSize: 16.0);
-      }
-    }
-  }
-
-  Future<bool> hasNetwork() async {
-    try {
-      final result = await InternetAddress.lookup('example.com');
-      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
-    } on SocketException catch (_) {
-      return false;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,11 +40,13 @@ class FeatureItemWidget extends StatelessWidget {
             Positioned(
               top: 32,
               left: 120,
-              child: SvgPicture.asset(
-                'assets/images/cinvu-logo.svg',
-                height: 64,
-                width: 64,
-              ),
+              child: Opacity(
+                  opacity: (feature.isEnable) ? 1 : 0.3,
+                  child: SvgPicture.asset(
+                    'assets/images/cinvu-logo.svg',
+                    height: 64,
+                    width: 64,
+                  )),
             ),
             Center(
               child: Text(
