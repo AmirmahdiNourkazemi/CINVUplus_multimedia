a2f6a52acb9915a24d0afc37701d5623013fc9ee
Sormeyli <mohammadhadisormeyli@gmail.com>
2023-06-03 12:57:27 +0330

    add multi languages feature

---
+++ lib/lang/app_localizations.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+import 'app_localizations_delegate.dart';
+
+class AppLocalizations {
+  late Locale locale;
+  late Map<String, String> _valueText;
+
+  AppLocalizations(this.locale);
+
+  static AppLocalizations of(BuildContext context) {
+    return Localizations.of(context, AppLocalizations);
+  }
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      AppLocalizationsDelegate();
+
+  Future loadTranslateFile() async {
+    String _langFile =
+        await rootBundle.loadString('assets/lang/${locale.languageCode}.json');
+
+    Map<String, dynamic> _json = jsonDecode(_langFile);
+    _valueText = _json.map((key, value) => MapEntry(key, value.toString()));
+  }
+
+  String getTranslate(String key) {
+    return _valueText[key]!;
+  }
+}
---
+++ lib/lang/app_localizations_delegate.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+import 'app_localizations.dart';
+
+class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+  const AppLocalizationsDelegate();
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'ar'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations appLocalizations = AppLocalizations(locale);
+    await appLocalizations.loadTranslateFile();
+    return appLocalizations;
+  }
+
+  @override
+  bool shouldReload(covariant AppLocalizationsDelegate old) => false;
+}
